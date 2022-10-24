e72ea6cdb684273192036ff7dbd489502862b603
Michael Maeng <mmaeng@amazon.com>
2022-10-24 11:01:59 -0400

    Check config file for useDocker and use_docker

--- python/rpdk/typescript/codegen.py
+++ python/rpdk/typescript/codegen.py
@@ -53,7 +53,7 @@ class TypescriptLanguagePlugin(LanguagePlugin):
         self.namespace = None
         self.package_name = None
         self.package_root = None
-        self._use_docker = True
+        self._use_docker = None
         self._protocol_version = "2.0.0"
         self._build_command = None
         self._lib_path = None
@@ -61,7 +61,8 @@ class TypescriptLanguagePlugin(LanguagePlugin):
     def _init_from_project(self, project):
         self.namespace = tuple(s.lower() for s in project.type_info)
         self.package_name = "-".join(self.namespace)
-        self._use_docker = project.settings.get("useDocker", True)
+        # Check config file for (legacy) 'useDocker' and use_docker settings
+        self._use_docker = project.settings.get("useDocker") or project.settings.get("use_docker")
         self.package_root = project.root / "src"
         self._build_command = project.settings.get("buildCommand", None)
         self._lib_path = SUPPORT_LIB_VERSION
@@ -74,7 +75,8 @@ class TypescriptLanguagePlugin(LanguagePlugin):
             "This is highly recommended unless you are experienced \n"
             "with cross-platform Typescript packaging.",
         )
-        project.settings["useDocker"] = self._use_docker
+        # Initially was 'useDocker' switched to 'use_docker' to be in line with python version
+        project.settings["use_docker"] = self._use_docker
         project.settings["protocolVersion"] = self._protocol_version
 
     def init(self, project):
